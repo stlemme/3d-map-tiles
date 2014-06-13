c372ee4f839a9a9f263e67cc08c0caca244f4628
Stefan Lemme <stefan.lemme@dfki.de>
2014-06-13 20:24:49 +0200

    initial version
    * add support for osm 3D tiles

---
+++ model.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!isset($_GET['x']) || !isset($_GET['y']) || !isset($_GET['z']) || !isset($_GET['provider'])) {
+	header('HTTP/1.1 404 Not Found');
+	die('Missing parameters!');
+}
+
+$x = intval($_GET['x']);
+$y = intval($_GET['y']);
+$z = intval($_GET['z']);
+
+$provider = $_GET['provider'];
+
+header('HTTP/1.1 200 Found');
+header('Content-type: application/xml');
+
+?>
+<?xml version="1.0" encoding="UTF-8"?>
+<xml3d xmlns="http://www.xml3d.org/2009/xml3d">
+	<model src="<?php echo $y; ?>.xml#asset" transform="<?php echo $y; ?>.xml#tf"/>
+</xml3d>
