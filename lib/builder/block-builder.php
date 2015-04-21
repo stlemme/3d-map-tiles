f558771a3aae39c31348c2bb955dd9de4a8815b8
Steffen Altmeier <steffen.altmeier@gmail.com>
2015-04-21 23:00:30 +0200

    triangulation is bugged, builder works.

--- lib/builder/block-builder.php
+++ lib/builder/block-builder.php
@@ -2,6 +2,7 @@
 
 
 require_once(__DIR__ . '/../geometry-builder.php');
+require_once(__DIR__ . '/../triangulation.php');
 
 
 class BlockBuilder extends GeometryBuilder
@@ -19,11 +20,29 @@ class BlockBuilder extends GeometryBuilder
 		// export as extruded triangulated polygon
 		$mesh->setShader($this->shader->getReference('building'));
 		$mesh->setMeshtype('triangles');
-		$data = $mesh->addData();
-		$data->compute("dataflow['" . $this->dataflow->getReference('extrude') . "']");
-		$data->addChild(new Float2('contour', $vertices));
 		$height = ($options['height'] > 0) ? $options['height'] : $this->height;
-		$data->addChild(new Float('height', array($height)));
+		$pos1 = Triangulate::ensureCCWContour($vertices);
+		$pos2 = Triangulate::planeXZ($pos1);
+		$ret= Triangulate::extrudePolygon($pos2, $height);
+		$pos3=$ret[0];
+		$index=$ret[1];
+		$position=Triangulate::deindex($pos3,$index);
+		$normal=Triangulate::generateFaceNormal($position);
+		/*	pos1 = xflow.ensureCCWContour(contour);
+			pos2 = xflow.planeXZ(pos1);
+			pos3, index = xflow.extrudePolygon(pos2, height);
+			position = xflow.deindex(pos3, index);
+			normal = xflow.generateFaceNormal(position);
+		*/
+		$data = $mesh->addData();
+		$data->addChild(new Float3('position', $position));
+		$data->addChild(new Float3('normal', $normal));
+		
+		//$data = $mesh->addData();
+		//$data->compute("dataflow['" . $this->dataflow->getReference('extrude') . "']");
+		//$data->addChild(new Float2('contour', $vertices));
+		//$height = ($options['height'] > 0) ? $options['height'] : $this->height;
+		//$data->addChild(new Float('height', array($height)));
 	}
 }
 
